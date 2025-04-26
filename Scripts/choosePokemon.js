b5d0b0ee97cb95f89f86c2b0b8b65dae53c5fcae
tanmaygupta-iitr <realtg140906@gmail.com>
2025-04-26 21:47:59 +0530

    ChoosePokemonFileUpgraded

--- Scripts/choosePokemon.js
+++ Scripts/choosePokemon.js
@@ -1,4 +1,3 @@
-const PlayerPokemon=[];
 const audio1=new Audio();
 audio1.src="./Assets/assets_opening.mp3";
 window.addEventListener('DOMContentLoaded',()=>{
@@ -16,7 +15,8 @@ async function getPokemondata(pokemonId){
             name:data.forms[0].name,
             img:{
                 front:data.sprites.front_default,
-                back:data.sprites.back_default,},
+                back:data.sprites.back_default,
+            },
             hp: data.stats.find(stat => stat.stat.name === 'hp').base_stat,
             damage: data.stats.find(stat => stat.stat.name === 'attack').base_stat,
             moves:{
@@ -39,73 +39,51 @@ async function getPokemondata(pokemonId){
             }
         }
         console.log("Useful data Extracted succesfully!");
-        // console.log(PokemonData);
-        console.log(PokemonData.img);
         return PokemonData;
     } catch (error) {
         console.log(error);
     }
 }
-const pokemonData1=await getPokemondata(1);
-const pokemonData2=await getPokemondata(7);
-const pokemonData3=await getPokemondata(4);
-
-
-var pokemonImg1=document.getElementById("pokemon-image1");
-var pokemonImg2=document.getElementById("pokemon-image2");
-var pokemonImg3=document.getElementById("pokemon-image3");
-
-pokemonImg1.src=`${pokemonData1.img.front}`;
-pokemonImg2.src=`${pokemonData2.img.front}`;
-pokemonImg3.src=`${pokemonData3.img.front}`;
-
-
-const p1=document.querySelector(".p1");
-const p2=document.querySelector(".p2");
-const p3=document.querySelector(".p3");
-const h1=document.querySelector(".h1");
-const enter=document.querySelector(".enter");
-let c=0;
-p1.addEventListener('click', function() {
-    h1.innerHTML="You Chose BULBASAUR!";
-    PlayerPokemon.push(pokemonData1);
-    p2.style.display="none";
-    p3.style.display="none";
-    setTimeout(()=>{
-        enter.style.display="block";
-    },1500)
-    c++;
-    console.log(PlayerPokemon);
-});
-
-p2.addEventListener('click', function() {
-    h1.innerHTML="You Chose SQUIRTLE!";
-    PlayerPokemon.push(pokemonData2);
-    p1.style.display="none";
-    p3.style.display="none";
-    setTimeout(()=>{
-        enter.style.display="block";
-    },1500)
-    c++;
-    console.log(PlayerPokemon);
-});
-
-p3.addEventListener('click', function() {
-    h1.innerHTML="You Chose CHARMANDER!";
-    PlayerPokemon.push(pokemonData3);
-    p2.style.display="none";
-    p1.style.display="none";
-    setTimeout(()=>{
-        enter.style.display="block";
-    },1500)
-    c++;
-    console.log(PlayerPokemon);
-});
-
-window.addEventListener("keydown",(e)=>{
-    if(e.key==="Enter" && c>=1){
+const PlayerPokemon=[];
+const ChoicesArr=[];
+const pokemonContainer=document.getElementsByClassName("pokemon_container")[0];
+for(let i=0;i<12;i++){
+    let randomNo=Math.floor(Math.random()*100)+1;
+    let randomNo2=Math.floor(Math.random()*100)+1;
+    for(let i=0;i<ChoicesArr.length;i++){
+        if(randomNo===ChoicesArr[i].id){
+            randomNo=randomNo2;
+            break;
+        }
+    }
+    const pokemonData=await getPokemondata(randomNo);
+    ChoicesArr.push(pokemonData);
+    const myh3=document.createElement("h3");
+    myh3.classList.add("h3");
+    myh3.textContent=`${pokemonData.name}`;
+    const myImg=document.createElement("img");
+    myImg.src=`${ChoicesArr[i].img.front}`;
+    console.log(myImg.src);
+    myImg.classList.add("img");
+    const myDiv=document.createElement("div");
+    myDiv.classList.add("pokemon");
+    myDiv.appendChild(myImg);
+    myDiv.appendChild(myh3); 
+    pokemonContainer.appendChild(myDiv);  
+    myDiv.addEventListener('click',()=>{
+        if(PlayerPokemon.length<3){
+            PlayerPokemon.push(pokemonData);
+            myDiv.style.backgroundColor="gray";
+        }
+    })
+}
+const selectBtn=document.getElementsByClassName("selectBtnDiv")[0];
+selectBtn.addEventListener('click',()=>{
+    if(PlayerPokemon.length!==3){
+    alert("You must Select 3 Pokemons!");  
+    }
+    else{
         localStorage.setItem("playerPokemon",JSON.stringify(PlayerPokemon));
         window.location.href="map.html";
-        // NEED TO APPLY LOCAL STORAGE AND BATTLE.js
     }
-});
+})
